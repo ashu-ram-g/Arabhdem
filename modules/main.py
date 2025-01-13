63b8eda1aae6d6959b73e9b061006a1694d03cac
Txtdatboy <herokukeliy@outlook.com>
2025-01-13 18:32:26 +0530

    Update main.py

--- modules/main.py
+++ modules/main.py
@@ -211,7 +211,7 @@ async def account_login(bot: Client, m: Message):
                         cmd = f'yt-dlp -o "{name}.zip" "{url}"'
                         download_cmd = f"{cmd} -R 25 --fragment-retries 25"
                         os.system(download_cmd)
-                        copy = await bot.send_document(chat_id=m.chat.id, document=f'{name}.pdf', caption=cc1)
+                        copy = await bot.send_document(chat_id=m.chat.id, document=f'{name}.zip', caption=cc1)
                         count += 1
                         os.remove(f'{name}.zip')
                     except FloodWait as e:
